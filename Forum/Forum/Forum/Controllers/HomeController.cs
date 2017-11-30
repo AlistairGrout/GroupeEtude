8b9c872e60c6fcf0f5dd5c3a1581e44ecba588b4
Pamellino <Pamellino@gmail.com>
2017-11-30 16:30:08 +0100

    test

--- Forum/Forum/Forum/Controllers/HomeController.cs
+++ Forum/Forum/Forum/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Forum.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your page.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contactttt page.";
 
             return View();
         }
